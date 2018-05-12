ea2687f8f2c0a5ec672a06750437675faae7286e
lazaryan <lazaryan99@gmail.com>
2018-05-12 11:40:40 +0300

    add form "add_visits"

--- js/main.min.js
+++ js/main.min.js
@@ -1,3 +1,19 @@
-/*
-    Здесь писать Js код
-*/
+/*Для бокового меню*/
+const nav 		= document.querySelector('#nav-js');
+
+nav.addEventListener("mouseout", (event) => {ShowNav(event.target, "out")});	//убрали курсор
+nav.addEventListener("mouseover", (event) => {ShowNav(event.target, "over")});	//навели
+
+function ShowNav(a, b){
+	const nav_elem 	= document.querySelectorAll('.nav-item');
+
+	if(b === "out"){
+		for( let i = 0; i < nav_elem.length; i++){
+			nav_elem[i].classList.add('nav-item_disactive');
+		}
+	}else {
+		for( let i = 0; i < nav_elem.length; i++){
+			nav_elem[i].classList.remove('nav-item_disactive');
+		}
+	}
+}
